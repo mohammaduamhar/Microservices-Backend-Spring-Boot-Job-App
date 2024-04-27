cfd13e7744d646d4ce0ef4fd585228012e9c97c6
mohammaduamhar <mamamhar254@gmail.com>
2024-04-27 12:29:57 +0530

    jobms micro service created

---
+++ jobms/src/main/java/com/amhar/jobms/job/JobController.java
@@ -0,0 +1,60 @@
+package com.amhar.jobms.job;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/jobs")
+public class JobController {
+    private  JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll(){
+        return  ResponseEntity.ok(jobService.findAll());
+    }
+    @PostMapping
+   public ResponseEntity<String> createJob(@RequestBody Job job) {
+        jobService.createJob(job);
+        return new ResponseEntity<>("job added successfully", HttpStatus.CREATED);
+    }
+
+
+@GetMapping("/{id}")
+        public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+
+         Job job = jobService.getJobById(id);
+         if (job != null) {
+             return new ResponseEntity<>(job, HttpStatus.OK );
+         }
+         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+
+
+
+        @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id) {
+      boolean deleted =  jobService.deleteJob(id);
+      if (deleted) {
+          return new ResponseEntity<>("job deleted successfully", HttpStatus.OK);
+      }
+        return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id, @RequestBody Job job) {
+        jobService.updateJob(id, job);
+        return new ResponseEntity<>("job updated successfully", HttpStatus.OK);
+    }
+
+
+
+
+}
