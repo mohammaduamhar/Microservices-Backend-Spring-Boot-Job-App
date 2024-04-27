01e3cd23082ae94d9dd1c03606721370a0b1d8cf
mohammaduamhar <mamamhar254@gmail.com>
2024-04-27 18:07:58 +0530

    review micro services created

---
+++ reviewms/src/test/java/com/amhar/reviewms/ReviewmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amhar.reviewms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
